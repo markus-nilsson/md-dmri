07116088eb39549bd6b09169f52b0ba3a31bc169
filip-szczepankiewicz <filip.szczepankiewicz@gmail.com>
2016-11-11 14:51:22 +0100

    Update gamma_opt.m
    Also changed the list of standard output parameters.

--- models/gamma/gamma_opt.m
+++ models/gamma/gamma_opt.m
@@ -16,7 +16,7 @@ opt.gamma = msf_ensure_field(opt.gamma, 'weight_sthresh', .05);
 opt.gamma = msf_ensure_field(opt.gamma, 'weight_wthresh', 2);
 opt.gamma = msf_ensure_field(opt.gamma, 'weight_mdthresh', 1e-9);
 opt.gamma = msf_ensure_field(opt.gamma, 'fig_maps', ...
-    {'s0', 'MD', 'MKi', 'MKa', 'MKt', 'ufa'}); 
+    {'s0', 'MD', 'MKi', 'MKa', 'MKt', 'ufa'});  
 opt.gamma = msf_ensure_field(opt.gamma, 'fig_prefix', 'gamma');
 opt.gamma = msf_ensure_field(opt.gamma, 'fit_iters', 1);
 opt.gamma = msf_ensure_field(opt.gamma, 'do_random_guess', 0);
