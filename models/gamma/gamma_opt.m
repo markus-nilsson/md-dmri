c9d52d7ca3aa96bf2e872974a7a6c186f6c60cc5
filip-szczepankiewicz <filip.szczepankiewicz@gmail.com>
2016-11-11 14:49:26 +0100

    Update gamma_opt.m
    Added controller for repeated fitting, random guess, powder average weight (to compensate for heteroscedasticity).
    Changed max iter to 500, weight_sthresh to 0.05.

--- models/gamma/gamma_opt.m
+++ models/gamma/gamma_opt.m
@@ -7,13 +7,16 @@ opt.gamma.present = 1;
 
 opt.gamma = msf_ensure_field(opt.gamma, 'tmp', 1); 
 opt.gamma = msf_ensure_field(opt.gamma, 'lsq_opts', ...
-    optimoptions('lsqcurvefit', 'display', 'off','MaxFunEvals',1e3));
+    optimoptions('lsqcurvefit', 'display', 'off','MaxFunEvals',500));
 opt.gamma = msf_ensure_field(opt.gamma, 'do_plot', 0);
 opt.gamma = msf_ensure_field(opt.gamma, 'do_pa', 0);
 opt.gamma = msf_ensure_field(opt.gamma, 'do_weight', 0);
-opt.gamma = msf_ensure_field(opt.gamma, 'weight_sthresh', .2);
-opt.gamma = msf_ensure_field(opt.gamma, 'weight_wthresh', 5);
+opt.gamma = msf_ensure_field(opt.gamma, 'do_pa_weight', 1);
+opt.gamma = msf_ensure_field(opt.gamma, 'weight_sthresh', .05);
+opt.gamma = msf_ensure_field(opt.gamma, 'weight_wthresh', 2);
 opt.gamma = msf_ensure_field(opt.gamma, 'weight_mdthresh', 1e-9);
 opt.gamma = msf_ensure_field(opt.gamma, 'fig_maps', ...
-    {'s0','iso','mu2iso','mu2aniso','mvlambda','ufa','ciso','cmu'});
+    {'s0', 'MD', 'MKi', 'MKa', 'MKt', 'ufa'}); 
 opt.gamma = msf_ensure_field(opt.gamma, 'fig_prefix', 'gamma');
+opt.gamma = msf_ensure_field(opt.gamma, 'fit_iters', 1);
+opt.gamma = msf_ensure_field(opt.gamma, 'do_random_guess', 0);
