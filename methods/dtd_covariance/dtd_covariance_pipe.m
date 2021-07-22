6f3f08b45417d3d2f55237a5a0fec7281b9818ec
filip-szczepankiewicz <filip.szczepankiewicz@gmail.com>
2021-07-22 10:17:54 +0200

    Added index control

--- methods/dtd_covariance/dtd_covariance_pipe.m
+++ methods/dtd_covariance/dtd_covariance_pipe.m
@@ -1,4 +1,4 @@
-function fn = dtd_covariance_pipe(s, paths, opt)
+function fn = dtd_covariance_pipe(s, paths, opt, ind)
 % function fn = dtd_covariance_pipe(s, paths, opt)
 %
 % s     - input structure
@@ -10,6 +10,7 @@ function fn = dtd_covariance_pipe(s, paths, opt)
 
 if ( (nargin < 2) || isempty(paths) ), paths = fileparts(s.nii_fn); end
 if (nargin < 3), opt.present = 1; end
+if (nargin < 4), ind = ones(s.xps.n, 1, 'logical'); end
 
 % Init structures
 opt   = mdm_opt(opt);
@@ -27,7 +28,7 @@ s = mdm_s_smooth(s, opt.filter_sigma, paths.nii_path, opt);
 s = mdm_s_mask(s, @mio_mask_threshold, paths.nii_path, opt);
 
 % Fit and derive parameters
-mdm_data2fit(@dtd_covariance_4d_data2fit, s, paths.mfs_fn, opt);
+mdm_data2fit(@dtd_covariance_4d_data2fit, s, paths.mfs_fn, opt, ind);
 mdm_fit2param(@dtd_covariance_4d_fit2param, paths.mfs_fn, paths.dps_fn, opt);
 
 % Save niftis
